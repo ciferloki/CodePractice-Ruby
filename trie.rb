994accaedddf282228c0dcf3fcf493b49bda9283
YueZhao <zhaoy1030@gmail.com>
2014-04-12 18:27:46 -0700

    Adding method to remove a word if exists, and to print the words starting from a given node.

--- trie.rb
+++ trie.rb
@@ -14,10 +14,10 @@ class Trie
     if key.nil? or key.empty?
       return true
     end
+    node = self
     len = key.length
     i = 0
-    node = self
-    while(i < len)
+    while (i < len)
       if not node.children.has_key? key[i]
         return false
       end
@@ -35,7 +35,7 @@ class Trie
     node = self
     len = key.length
     i = 0
-    while(i < len)
+    while (i < len)
       if not node.children.has_key? key[i]
         break
       end
@@ -43,7 +43,7 @@ class Trie
       i += 1
     end
 
-    while(i < len)
+    while (i < len)
       new_node = Trie.new(key[i])
       node.children[key[i]] = new_node
       node = new_node
@@ -51,10 +51,33 @@ class Trie
     end
   end
 
+  # Recursively remove string from the trie
+  def remove(key)
+    if key.nil? or key.empty?
+      return true
+    end
+    if not @children.has_key? key[0] or not @children[key[0]].remove(key[1, key.length - 1])
+      return false
+    end
+    if @children[key[0]].children.empty?
+      @children.delete(key[0])
+    end
+    return true
+  end
+
+  def print_node(prefix)
+    str = prefix + key
+    puts str
+    @children.each do |key, value|
+      value.print_node(str)
+    end
+  end
+
 end
 
 trie = Trie.new('')
-trie.insert(' ')
-trie.insert('dhariabi')
 trie.insert('nitingup')
-puts trie.lookup(' ')
+trie.insert('nitinjup')
+trie.remove('nitinjup')
+trie.print_node('')
+puts trie.lookup('n')
